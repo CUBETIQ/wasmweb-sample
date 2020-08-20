2e007a3bae9e9559858bfb1c2f5adca19919537f
Sambo Chea <sombochea@cubetiqs.com>
2020-08-20 11:42:43 +0700

    Typescript support for running web

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -2,10 +2,23 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-  entry: "./bootstrap.js",
+  entry: "./bootstrap.ts",
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
+    filename: 'bundle.js',
   },
   mode: "development",
   plugins: [
